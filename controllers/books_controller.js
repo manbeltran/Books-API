2159ebf0135f415d40d96867e4c065432d283529
Jesus Beltran <jesusbeltran@Jesuss-MBP.attlocal.net>
2022-08-27 01:38:46 -0700

    json not displaying

--- controllers/books_controller.js
+++ controllers/books_controller.js
@@ -1,2 +1,14 @@
 // dependencies
 const express = require('express')
+const Book = require('../models/book.js')
+const books = express.Router()
+
+
+
+// Index?
+books.get('/', async (req, res) => {
+    
+})
+
+//export
+module.exports = books
--- index.js
+++ index.js
@@ -15,17 +15,20 @@ app.use(express.json())
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
     () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
   )
-  
+
 // Routes
 app.get('/', (req, res) => {
     res.send('Welcome to an awesome Book Application!')
 })
 
+//books
+const booksController = require('./controllers/books_controller.js')
+app.use('/books', booksController)
+
+//404 page
 app.get('*', (req,res) => {
     res.status(404).send('<h1>404</h1>')
 })
-
-
 
 //Listen
 app.listen(PORT, () => {
